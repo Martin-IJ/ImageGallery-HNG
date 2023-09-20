4b4af3793dba175f1e0a39e5efef04086a90a62b
martins <martinohez@gmail.com>
2023-09-20 23:03:18 +0100

    updated dnd

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {useState, useEffect} from "react";
 import { useGlobalContext } from "../Context";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const ImageCard = () => {
   const { filteredImages, errorMessage, setFilteredImages } =
     useGlobalContext();
-  const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
@@ -14,60 +15,78 @@ const ImageCard = () => {
     }, 2000);
   }, []);
 
-  const dragItem = useRef(null);
-  const dragOverItem = useRef(null);
+  const handleDragDrop = (results) => {
+    const { source, destination, type } = results;
 
-  const handleSort = () => {
-    if (dragItem.current !== null && dragOverItem.current !== null) {
-      const _filteredImages = [...filteredImages];
-      const draggedItemContent = _filteredImages.splice(dragItem.current, 1)[0];
-      _filteredImages.splice(dragOverItem.current, 0, draggedItemContent);
+    if (!destination) return;
 
-      dragItem.current = null;
-      dragOverItem.current = null;
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    )
+      return;
 
-      setFilteredImages(_filteredImages);
+    if (type === "group") {
+      const reorderedimages = [...filteredImages];
+      const sourceIndex = source.index;
+      const destinationIndex = destination.index;
+      const [removedImages] = reorderedimages.splice(sourceIndex, 1);
+      reorderedimages.splice(destinationIndex, 0, removedImages);
+
+      return setFilteredImages(reorderedimages);
     }
   };
 
   return (
     <div className="px-4 my-5">
-      <h1 className="text-3xl font-semibold uppercase tracking-wider">
-        iCe Image Gallery
-      </h1>
-      {isLoading ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 mt-6">
-          {[1, 2, 3, 4].map((index) => (
-            <div key={index}>
-              <Skeleton width={310} height={320} />
+      <DragDropContext onDragEnd={handleDragDrop}>
+        <h1 className="text-3xl font-semibold uppercase tracking-wider">
+          iCe Image Gallery
+        </h1>
+        <Droppable droppableId="ROOT" type="group">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {isLoading ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 mt-6">
+                  {[1, 2, 3, 4].map((index) => (
+                    <div key={index}>
+                      <Skeleton width={310} height={320} />
+                    </div>
+                  ))}
+                </div>
+              ) : filteredImages.length === 0 ? (
+                <p className="m-auto mt-10 text-3xl">{errorMessage}</p>
+              ) : (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-6">
+                  {filteredImages.map((image, index) => (
+                    <Draggable
+                      draggableId={image.id.toString()}
+                      index={index}
+                      key={image.id}
+                    >
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          className={snapshot.isDragging ? "dragging" : ""}
+                        >
+                          <img
+                            src={image.image}
+                            alt="ImageCard"
+                            className="w-full h-full object-cover object-center"
+                          />
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </div>
+              )}
+              {provided.placeholder}
             </div>
-          ))}
-        </div>
-      ) : filteredImages.length === 0 ? (
-        <p className="m-auto mt-10 text-3xl">{errorMessage}</p>
-      ) : (
-        <div
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-6"
-          onDragOver={(e) => e.preventDefault()}
-        >
-          {filteredImages.map((image, index) => (
-            <div
-              key={image.id}
-              className="cursor-grab dragging"
-              draggable
-              onDragStart={(e) => (dragItem.current = index)}
-              onDragEnter={(e) => (dragOverItem.current = index)}
-              onDragEnd={handleSort}
-            >
-              <img
-                src={image.image}
-                alt="ImageCard"
-                className="w-full h-full object-cover object-center"
-              />
-            </div>
-          ))}
-        </div>
-      )}
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
